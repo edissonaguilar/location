9bee6f6203e381cb2a4ff15aa5e9a264ee4bdbf0
Edisson <edissonaguilar@outlook.com>
2019-12-27 11:57:50 -0500

    agregando estados hn

---
+++ countries/hn/states.js
@@ -0,0 +1,128 @@
+[
+  {
+    Hash: 'hn-at',
+    Id: 1,
+    Name: 'Atlántida',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-co',
+    Id: 2,
+    Name: 'Colón',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-cy',
+    Id: 3,
+    Name: 'Comayagua',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-cp',
+    Id: 4,
+    Name: 'Copán',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-cr',
+    Id: 5,
+    Name: 'Cortés',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-ch',
+    Id: 6,
+    Name: 'Choluteca',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-ep',
+    Id: 7,
+    Name: 'El Paraíso',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-fm',
+    Id: 8,
+    Name: 'Francisco Morazán',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-gd',
+    Id: 9,
+    Name: 'Gracias a Dios',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-in',
+    Id: 10,
+    Name: 'Intibucá',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-ib',
+    Id: 11,
+    Name: 'Islas de la Bahía',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-lp',
+    Id: 12,
+    Name: 'La Paz',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-le',
+    Id: 13,
+    Name: 'Lempira',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-oc',
+    Id: 14,
+    Name: 'Ocotepeque',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-ol',
+    Id: 15,
+    Name: 'Olancho',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-sb',
+    Id: 16,
+    Name: 'Santa Bárbara',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-va',
+    Id: 17,
+    Name: 'Valle',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  },
+  {
+    Hash: 'hn-yo',
+    Id: 18,
+    Name: 'Yoro',
+    Country: { Hash: 'hn', Id: 340, Name: 'Honduras' },
+    Cities: []
+  }
+]
